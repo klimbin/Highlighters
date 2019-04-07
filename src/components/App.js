c5fe2b7be6b243f9ed70e457238f87572eeac8e7
Kevin Lim <klim560@gmail.com>
2019-04-07 00:40:54 -0700

    Ported code to React App

---
+++ src/components/App.js
@@ -0,0 +1,193 @@
+import React, { Component } from 'react';
+import HighlightPanel, { Highlight } from './Highlight';
+
+/** Defines the text of the page to search through.
+ * @constant
+ */
+const pageText = `
+  This is a test.</br>
+  You will deliver new technology with an adorable puppy. Perfect!</br>
+  This is just a test.</br>
+  You will deliver new technology with an adorable puppy. Perfect!</br>
+  I love chocolate.</br>
+  You will deliver new technology with an adorable puppy. Perfect!</br>
+  You will deliver new technology with an adorable puppy. Perfect!</br>`;
+
+const INITIAL_STATE = {
+  inputText: pageText,
+  phrase: 'You will deliver new technology with an adorable puppy. Perfect!',
+  highlights: [
+    new Highlight(29,30,'orange',0),
+    new Highlight(24,27,'#eccf98',1),
+    new Highlight(4, 18, '#d9f593', 2),
+    new Highlight(19, 22, '#e8e8e8', 3),
+    new Highlight(17, 31, '#c6e5fa', 4),
+    new Highlight(40, 48, '#d9f593', 5),
+    new Highlight(40, 55, '#ee93f5', 6),
+    new Highlight(37, 55, '#c6e5fa', 7),
+    new Highlight(56, 64, '#eccf98', 8),
+    new Highlight(-200, 200, 'yellow', 90)
+  ],
+  highlightRender: null
+};
+
+/** This class will render the highlights for the phrase
+ *  on every occurrence in the pageText.
+ */
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE};
+  }
+
+  componentDidMount() {
+    this.updateRenders();
+  }
+
+  /**
+   * This method takes a key unique to a highlight object
+   * then removes the given highlight from the App state.
+   * Note that we use the highlight priority for the key.
+   *
+   * @param {Number.} key
+   */
+  removeHighlight = (key) => {
+    const { highlights } = this.state;
+    const newHighlights = highlights.filter((h) => h.priority !== key);
+    this.setState({ highlights: newHighlights }, this.updateRenders);
+  }
+
+  /**
+   * This method clears the previous renders
+   * and creates new highlight renders.
+   *
+   * @param {Number.} key
+   */
+  updateRenders = () => {
+    const { inputText, phrase, highlights, highlightRender } = this.state;
+    const renders = calculateRenders(phrase.length, highlights);
+    const newRender = getHighlightedText(phrase, renders);
+    this.setState({ highlightRender: inputText.split(phrase).join(newRender) });
+  }
+
+  render() {
+    const { phrase, highlights, highlightRender } = this.state;
+    return(
+      <div>
+        <div dangerouslySetInnerHTML={{__html: highlightRender}}></div>
+        <HighlightPanel
+          phrase={phrase}
+          highlights={highlights}
+          highlightChange={this.removeHighlight}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
+
+/**
+ * This function takes an array of highlight objects and
+ * and normalizes the ranges to create a set of highlight
+ * renders without any overlapping.
+ *
+ * Highlight renders are arrays represented by [start, end, color, priority].
+ *
+ * @param {Number} inputLength - The length of the phrase to be highlighted.
+ * @param {Array.<Highlight>} highlights - A set of highlight objects.
+ *
+ * @return {Array.<Array.<Number, Number, String, Number>>} An array of highlight renders.
+ */
+const calculateRenders = (inputLength, highlights) => {
+  // sort highlights from top to bottom by their priority
+  // note that lower numbers are higher in priority
+  highlights.sort((a,b) => a.priority - b.priority);
+
+  // renders will store set of highlight objects that do not overlap
+  let renders = [];
+
+  // this string will represent which indices are already highlighted
+  let spotsFilled = '0'.repeat(inputLength);
+
+  for (let i = 0; i < highlights.length; i++) {
+    let current = highlights[i];
+    let [start, end] = [current.startOffset, current.endOffset];
+
+    // ignore current highlight if it does not have a valid range
+    if(start >= end || end <= 0 || start >= inputLength) {
+      continue;
+    }
+    if(start < 0) {
+      start = 0;
+    }
+    if(end > inputLength) {
+      end = inputLength;
+    }
+
+    let [color, priority] = [current.color, current.priority];
+    let sectionToConsider = spotsFilled.substring(start, end);
+    let firstOccurrence = sectionToConsider.indexOf('1');
+
+    if(firstOccurrence === -1) {
+      // no overlaps with other highlights
+      renders.push([start, end, color, priority]);
+    }
+    else {
+      // there is overlapping with other highlight(s)
+      if(firstOccurrence > 0) {
+        renders.push([start, start+firstOccurrence, color, priority]);
+      }
+
+      let lastOccurrence = sectionToConsider.lastIndexOf('1');
+
+      // handle cases of multiple highlight overlaps
+      let nextOccurrence = sectionToConsider.indexOf('1', firstOccurrence+1);
+      while(nextOccurrence !== -1) {
+        if(nextOccurrence - firstOccurrence > 1) {
+          renders.push([start+firstOccurrence+1, start+nextOccurrence, color, priority]);
+        }
+        firstOccurrence = nextOccurrence;
+        nextOccurrence = sectionToConsider.indexOf('1', firstOccurrence+1);
+      }
+
+      if(lastOccurrence < sectionToConsider.length-1) {
+        renders.push([start+lastOccurrence+1, end, color, priority]);
+      }
+    }
+    spotsFilled = spotsFilled.substring(0, start) + '1'.repeat(end-start) + spotsFilled.substring(end);
+  }
+  return renders;
+}
+
+/**
+ * This function takes a set of highlight renders
+ * and returns HTML code to represent the highlights.
+ * The highlight renders must not have overlaps.
+ *
+ * @param {string} string - The phrase to be highlighted.
+ * @param {Array.<Array.<Number, Number, String, Number>>} sections
+ *
+ * Highlight renders are represented by [start, end, color, priority].
+ *
+ * @return {string} HTML markup representing the highlighted phrase.
+ */
+const getHighlightedText = (string, sections) => {
+  // sort sections by increasing startOffset
+  sections.sort((a,b) => a[0] - b[0]);
+
+  // append each section with their highlights
+  let result = [];
+  let currentIndex = 0;
+  for(let i = 0; i < sections.length; i++) {
+    let [start, end, color, priority] = sections[i];
+    let section = string.substring(currentIndex, start);
+    section += `<span class="render-${priority}"` +
+               ` style="background-color:${color}">` +
+               `${string.substring(start, end)}</span>`;
+    result.push(section);
+    currentIndex = end;
+  }
+  result.push(string.substring(currentIndex));
+  return result.join('');
+}
---
+++ src/components/Highlight.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import './Highlight.css';
+
+/** Class representing a highlight. */
+const Highlight = class {
+  /**
+   * Create a Highlight object.
+   * @param {number} startOffset - The startOffset value.
+   * @param {number} endOffset - The endOffset value.
+   * @param {string} color - The highlight color.
+   * @param {number} priority - The priority of the highlight.
+   */
+  constructor(startOffset, endOffset, color, priority) {
+      this.startOffset = startOffset;
+      this.endOffset = endOffset;
+      this.color = color;
+      this.priority = priority;
+  }
+
+  /**
+   * @return {string} A string representing this Highlight object.
+   */
+  toString() {
+      return `Highlight: (${this.startOffset},${this.endOffset})` +
+              `, ${this.color}, priority = ${this.priority}`;
+  }
+
+  /**
+   * Get the startOffset value.
+   * @return {number} The startOffset value.
+   */
+  get startOffset() {
+      return this._startOffset;
+  }
+
+  /**
+   * Get the endOffset value.
+   * @return {number} The endOffset value.
+   */
+  get endOffset() {
+      return this._endOffset;
+  }
+
+  /**
+   * Get the color value.
+   * @return {string} The color value.
+   */
+  get color() {
+      return this._color;
+  }
+
+  /**
+   * Get the priority value.
+   * @return {number} The priority value.
+   */
+  get priority() {
+      return this._priority;
+  }
+
+  /**
+   * Set the startOffset value.
+   * @param {number} The startOffset value.
+   */
+  set startOffset(value) {
+      this._startOffset = value;
+  }
+
+  /**
+   * Set the endOffset value.
+   * @param {number} The endOffset value.
+   */
+  set endOffset(value) {
+      this._endOffset = value;
+  }
+
+  /**
+   * Set the color value.
+   * @param {string} The color value.
+   */
+  set color(value) {
+      this._color = value;
+  }
+
+  /**
+   * Set the priority value.
+   * @param {number} The priority value.
+   */
+  set priority(value) {
+      this._priority = value;
+  }
+};
+
+/** This class renders boxes to represent each highlight object. */
+class HighlightPanel extends Component {
+  /**
+   * This method takes a key unique to a highlight object
+   * then calls a callback function to remove the given highlight.
+   * Note that we use the highlight priority for the key.
+   *
+   * @param {Number.} key
+   */
+  deleteHighlight = (key) => {
+    this.props.highlightChange(key);
+  }
+
+  /**
+   * This method takes a highlight object and
+   * returns a div representing the highlight.
+   *
+   * @param {Object.<Highlight>} highlight
+   */
+  renderHighlightBox = (highlight) => {
+    const phrase = this.props.phrase;
+    const {startOffset, endOffset, color, priority} = highlight;
+    const classNames = ['card render-' + priority]
+    const style = {backgroundColor: color};
+    return (
+      <div key={priority} className={classNames} style={style}>
+        <div className="card-body">
+          <h5 className="card-title">
+            Highlight {priority}
+          </h5>
+          <h6 className="card-subtitle">
+            ({startOffset}, {endOffset})
+          </h6>
+          <div className="card-text">
+            <p>
+              "{phrase.substring(startOffset, endOffset)}"
+            </p>
+            <button
+              className="btn btn-outline-dark"
+              onClick={() => this.deleteHighlight(priority)}
+            >
+              <span aria-hidden="true">remove</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const highlightBoxes = this.props.highlights.map(highlight =>
+      this.renderHighlightBox(highlight)
+    );
+
+    return (
+      <div id="highlightBoxContainer">
+        { highlightBoxes }
+      </div>
+    );
+  }
+}
+
+export default HighlightPanel;
+export { Highlight };
